6c3433b480ad8306f4cfba8bdb4253d37478d4cc
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-07-14 09:41:51 +0530

    active and finshed project list rendering

--- src/app.ts
+++ src/app.ts
@@ -58,6 +58,40 @@ function autobind(
   return adjDescriptior;
 }
 
+// Project list class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector(
+      'h2'
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // Project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -154,4 +188,6 @@ class ProjectInput {
   }
 }
 
-const p = new ProjectInput();
+const projInput = new ProjectInput();
+const activeProjList = new ProjectList('active');
+const finishedProjList = new ProjectList('finished');
